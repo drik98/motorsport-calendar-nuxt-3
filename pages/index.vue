88cac21b6c218543ff1fa13736e8b0aa75dc9608
Hendrik Schmitz <hendrik.schmitz@aixigo.com>
2023-04-03 08:58:26 +0200

    Transform data and display as ul

--- pages/index.vue
+++ pages/index.vue
@@ -1,14 +1,24 @@
 <template>
-  <div class="hello-scss">
+  <div class="hello-scss p-2">
     <div class="hello-scss__element text-2xl">Hello World, {{ value }}!</div>
-    {{ data }}
+    <ul class="list-disc">
+      <li v-for="race in data" :key="race.name" class="my-2">
+        <strong>{{ race.name }}</strong>
+        <ul class="list-disc list-inside">
+          <li v-for="[ key, entry] in Object.entries( race )" :key="key">
+            {{ key }}: {{ entry }}
+          </li>
+        </ul>
+      </li>
+    </ul>
   </div>
 </template>
 
 <script setup lang="ts">
 import { Ref } from 'vue'
+import { Race } from '~/server/api/google-sheets'
 
-const { data } = useFetch('/api/google-sheets')
+const { data } = useFetch<Race[]>('/api/google-sheets')
 
 // Typescript ESLint check;
 type Value = string
--- server/api/google-sheets.ts
+++ server/api/google-sheets.ts
@@ -1,4 +1,58 @@
+import { sheets_v4 } from '@googleapis/sheets';
+import parser from 'any-date-parser';
+const dateParser = parser.exportAsFunction();
+
 export default defineEventHandler(async () => {
    const runtimeConfig = useRuntimeConfig()
-   return $fetch(`https://sheets.googleapis.com/v4/spreadsheets/${runtimeConfig.spreadsheetId}/values/${runtimeConfig.spreadsheetName}?key=${runtimeConfig.googleApiKey}`);
+   return $fetch(`https://sheets.googleapis.com/v4/spreadsheets/${runtimeConfig.spreadsheetId}/values/${runtimeConfig.spreadsheetName}?key=${runtimeConfig.googleApiKey}`)
+      .then( data => {
+         return toObjectArray( data as sheets_v4.Schema$ValueRange ).map( toRace )
+      })
 });
+
+type RowEntry = Record<string, string>;
+
+function toObjectArray( data: sheets_v4.Schema$ValueRange ): Array<RowEntry> {
+   if( !data.values ) {
+      return [];
+   }
+
+   const [ columnValues, ...values ] = data.values;
+   const columns = ( columnValues as string[] ).map(column => column.trim());
+
+   return values.map( row => {
+      const rowEntry: RowEntry = {  };
+      columns.forEach( (column, columnIndex) => {
+         rowEntry[column as string] = row[columnIndex];
+      });
+      return rowEntry;
+   })
+}
+
+export type Race = {
+   startDate: string
+   endDate?: string
+   startTime?: string
+   endTime?: string
+   host?: string
+   type?: string
+   name?: string
+   karting?: string
+   location?: string
+   info?: string
+} & Record<string, string>
+
+function toRace( rowEntry: RowEntry ): Race {
+   const { Datum, Rennen, Zeit, ...rest } = rowEntry;
+   const [ startDate, endDate ] = Datum?.split('-').map(date => dateParser(date))  ?? [];
+
+   const race: Race = {
+      ...rest,
+      startDate,
+      endDate,
+      name: Rennen,
+      time: Zeit
+   };
+
+   return race;
+}
