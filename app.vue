cd484d291deda7abb43414124702e9338f73c22b
Hendrik Schmitz <hendrik.schmitz@aixigo.com>
2023-08-05 14:08:14 +0200

    Add vuetify layout to app.vue

--- app.vue
+++ app.vue
@@ -1,7 +1,22 @@
 <template>
-   <NuxtPage/>
- </template>
- 
+  <v-app id="inspire">
+    <v-app-bar app clipped-left>
+      <v-toolbar-title>Motorsport Kalender</v-toolbar-title>
+    </v-app-bar>
+
+    <v-main>
+      <v-container class="fill-height" fluid>
+        <NuxtPage />
+      </v-container>
+    </v-main>
+
+    <v-footer app class="justify-space-between">
+      <span>&copy; {{ new Date().getFullYear() }}</span>
+      <span>Hendrik Schmitz</span>
+    </v-footer>
+  </v-app>
+</template>
+
 <style lang="scss">
-   @use './settings';
+@use './settings';
 </style>
